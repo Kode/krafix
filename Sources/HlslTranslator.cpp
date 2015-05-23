2dc49ae40d271567d57ba0949e949079a45c373c
Robert Konrad <robert@ktx-software.com>
2015-05-23 12:25:41 +0200

    Updated HlslTranslator.

--- Sources/HlslTranslator.cpp
+++ Sources/HlslTranslator.cpp
@@ -31,6 +31,10 @@ void HlslTranslator::outputCode(const Target& target, const char* filename, std:
 		outputInstruction(target, attributes, inst);
 		if (outputting) (*out) << "\n";
 	}
+	for (unsigned i = 0; i < functions.size(); ++i) {
+		(*out) << functions[i]->text.str();
+		(*out) << "\n\n";
+	}
 
 	file.close();
 }
@@ -39,6 +43,169 @@ void HlslTranslator::outputInstruction(const Target& target, std::map<std::strin
 	using namespace spv;
 
 	switch (inst.opcode) {
+	case OpLabel: {
+		if (firstLabel) {
+			output(out);
+			if (firstFunction) {
+				if (stage == EShLangVertex && target.version == 9) {
+					(*out) << "uniform float4 dx_ViewAdjust;";
+				}
+				(*out) << "\n";
+
+				std::vector<Variable> sortedVariables;
+				for (std::map<unsigned, Variable>::iterator v = variables.begin(); v != variables.end(); ++v) {
+					sortedVariables.push_back(v->second);
+				}
+				currentNames = names;
+				std::sort(sortedVariables.begin(), sortedVariables.end(), compareVariables);
+
+				for (unsigned i = 0; i < sortedVariables.size(); ++i) {
+					Variable variable = sortedVariables[i];
+
+					Type t = types[variable.type];
+					Name n = names[variable.id];
+
+					if (variable.storage == StorageClassUniformConstant) {
+						indent(out);
+						(*out) << "uniform " << t.name << " " << n.name << ";\n";
+					}
+				}
+				(*out) << "\n";
+
+				(*out) << "struct Input {\n";
+				++indentation;
+				if (stage == EShLangFragment && target.version > 9) {
+					indent(out);
+					(*out) << "float4 gl_Position : SV_POSITION;\n";
+				}
+				int index = 0;
+				for (unsigned i = 0; i < sortedVariables.size(); ++i) {
+					Variable variable = sortedVariables[i];
+
+					Type t = types[variable.type];
+					Name n = names[variable.id];
+
+					if (variable.storage == StorageClassInput) {
+						indent(out);
+						(*out) << t.name << " " << n.name << " : TEXCOORD" << index << ";\n";
+						if (stage == EShLangVertex) {
+							attributes[n.name] = index;
+						}
+						++index;
+					}
+				}
+				--indentation;
+				indent(out);
+				(*out) << "};\n\n";
+
+				indent(out);
+				(*out) << "struct Output {\n";
+				++indentation;
+				index = 0;
+
+				for (unsigned i = 0; i < sortedVariables.size(); ++i) {
+					Variable variable = sortedVariables[i];
+
+					Type t = types[variable.type];
+					Name n = names[variable.id];
+
+					if (variable.storage == StorageClassOutput) {
+						if (variable.builtin && stage == EShLangVertex) {
+							positionName = n.name;
+							indent(out);
+							if (target.version == 9) {
+								(*out) << t.name << " " << n.name << " : POSITION;\n";
+							}
+							else {
+								(*out) << t.name << " " << n.name << " : SV_POSITION;\n";
+							}
+						}
+						else if (variable.builtin && stage == EShLangFragment) {
+							indent(out);
+							(*out) << t.name << " " << n.name << " : COLOR;\n";
+						}
+					}
+				}
+
+				for (unsigned i = 0; i < sortedVariables.size(); ++i) {
+					Variable variable = sortedVariables[i];
+
+					Type t = types[variable.type];
+					Name n = names[variable.id];
+
+					if (variable.storage == StorageClassOutput) {
+						if (variable.builtin && stage == EShLangVertex) {
+
+						}
+						else if (variable.builtin && stage == EShLangFragment) {
+
+						}
+						else {
+							indent(out);
+							(*out) << t.name << " " << n.name << " : TEXCOORD" << index << ";\n";
+							++index;
+						}
+					}
+				}
+
+				--indentation;
+				indent(out);
+				(*out) << "};\n\n";
+				firstFunction = false;
+			}
+
+			if (funcName != "main") {
+				(*out) << funcType << " " << funcName << "(";
+				for (unsigned i = 0; i < parameters.size(); ++i) {
+					(*out) << parameters[i].type.name << " " << getReference(parameters[i].id);
+					if (i < parameters.size() - 1) (*out) << ", ";
+				}
+				(*out) << ");\n";
+			}
+
+			startFunction(funcName);
+
+			if (funcName == "main") {
+				(*out) << "Output main(Input input)\n";
+			}
+			else {
+				(*out) << funcType << " " << funcName << "(";
+				for (unsigned i = 0; i < parameters.size(); ++i) {
+					(*out) << parameters[i].type.name << " " << getReference(parameters[i].id);
+					if (i < parameters.size() - 1) (*out) << ", ";
+				}
+				(*out) << ")\n";
+			}
+			indent(out);
+			(*out) << "{";
+			++indentation;
+
+			if (funcName == "main") {
+				(*out) << "\n";
+				indent(out);
+				(*out) << "Output output;\n";
+				for (std::map<unsigned, Variable>::iterator v = variables.begin(); v != variables.end(); ++v) {
+					Variable variable = v->second;
+					if (variable.storage == StorageClassOutput) {
+						Type t = types[variable.type];
+						Name n = names[variable.id];
+						indent(out);
+						(*out) << "output." << n.name << " = ";
+						if (t.name == "float") (*out) << "0.0;\n";
+						if (t.name == "float2") (*out) << "float2(0.0, 0.0);\n";
+						if (t.name == "float3") (*out) << "float3(0.0, 0.0, 0.0);\n";
+						if (t.name == "float4") (*out) << "float4(0.0, 0.0, 0.0, 0.0);\n";
+					}
+				}
+			}
+
+			firstLabel = false;
+		}
+		else {
+			CStyleTranslator::outputInstruction(target, attributes, inst);
+		}
+		break;
+	}
 	case OpExecutionMode:
 		break;
 	case OpTypeArray: {
@@ -123,7 +290,7 @@ void HlslTranslator::outputInstruction(const Target& target, std::map<std::strin
 		}
 		break;
 	}
-	case OpFunction: {
+	/*case OpFunction: {
 		output(out);
 		if (stage == EShLangVertex && target.version == 9) {
 			(*out) << "uniform float4 dx_ViewAdjust;";
@@ -249,7 +416,7 @@ void HlslTranslator::outputInstruction(const Target& target, std::map<std::strin
 			}
 		}
 		break;
-	}
+	}*/
 	case OpCompositeConstruct: {
 		Type resultType = types[inst.operands[0]];
 		id result = inst.operands[1];
@@ -303,14 +470,18 @@ void HlslTranslator::outputInstruction(const Target& target, std::map<std::strin
 		(*out) << "return output;";
 		break;
 	case OpStore: {
-		output(out);
 		Variable& v = variables[inst.operands[0]];
-		if (!v.declared) {
-			(*out) << types[v.type].name << " " << getReference(inst.operands[0]) << " = " << getReference(inst.operands[1]) << ";";
-			v.declared = true;
+		if (getReference(inst.operands[0]) == "param") {
+			references[inst.operands[0]] = getReference(inst.operands[1]);
 		}
 		else {
-			(*out) << getReference(inst.operands[0]) << " = " << getReference(inst.operands[1]) << ";";
+			output(out);
+			if (compositeInserts.find(inst.operands[1]) != compositeInserts.end()) {
+				(*out) << getReference(inst.operands[0]) << indexName(compositeInserts[inst.operands[1]]) << " = " << getReference(inst.operands[1]) << ";";
+			}
+			else {
+				(*out) << getReference(inst.operands[0]) << " = " << getReference(inst.operands[1]) << ";";
+			}
 		}
 		break;
 	}
