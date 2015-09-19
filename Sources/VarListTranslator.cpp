4f1b17797689111fd958be04452efbf0153f5ef8
Robert Konrad <robert@ktx-software.com>
2015-09-19 23:45:39 +0200

    Added VarListTranslator.

---
+++ Sources/VarListTranslator.cpp
@@ -0,0 +1,72 @@
+#include "VarListTranslator.h"
+#include <fstream>
+
+using namespace krafix;
+
+namespace {
+	typedef unsigned id;
+
+	struct Name {
+		const char* name;
+	};
+
+	struct Type {
+		const char* name;
+		unsigned length;
+		bool isarray;
+
+		Type() : name("unknown"), length(1), isarray(false) {}
+	};
+
+	struct Variable {
+		unsigned id;
+		unsigned type;
+		spv::StorageClass storage;
+		bool builtin;
+
+		Variable() : builtin(false) {}
+	};
+}
+
+void VarListTranslator::outputCode(const Target& target, const char* filename, std::map<std::string, int>& attributes) {
+	using namespace spv;
+
+	std::map<unsigned, Name> names;
+	std::map<unsigned, Variable> variables;
+	std::map<unsigned, Type> types;
+
+	std::ofstream out;
+	out.open(filename, std::ios::binary | std::ios::out);
+
+	for (unsigned i = 0; i < instructions.size(); ++i) {
+		Instruction& inst = instructions[i];
+		switch (inst.opcode) {
+		case OpName: {
+			unsigned id = inst.operands[0];
+			if (strcmp(inst.string, "") != 0) {
+				Name n;
+				n.name = inst.string;
+				names[id] = n;
+			}
+			break;
+		}
+		case OpVariable: {
+			Type resultType = types[inst.operands[0]];
+			id result = inst.operands[1];
+			types[result] = resultType;
+			Variable& v = variables[result];
+			v.id = result;
+			v.type = inst.operands[0];
+			v.storage = (StorageClass)inst.operands[2];
+
+			if (names.find(result) != names.end()) {
+				out << names[result].name << "\n";
+			}
+
+			break;
+		}
+		}
+	}
+
+	out.close();
+}
---
+++ Sources/VarListTranslator.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Translator.h"
+
+namespace krafix {
+	class VarListTranslator : public Translator {
+	public:
+		VarListTranslator(std::vector<unsigned>& spirv, EShLanguage stage) : Translator(spirv, stage) {}
+		void outputCode(const Target& target, const char* filename, std::map<std::string, int>& attributes);
+	};
+}
