dcf54175b1fcd1278fab637b804e12494a5241ef
Polona Caserman <polona.caserman@gmail.com>
2017-01-11 14:34:20 +0100

    Format tabs

--- Sources/MetalTranslator2.cpp
+++ Sources/MetalTranslator2.cpp
@@ -5,24 +5,24 @@
 using namespace krafix;
 
 namespace {
-    std::string extractFilename(std::string path) {
-        int i = (int)path.size() - 1;
-        for (; i > 0; --i) {
-            if (path[i] == '/' || path[i] == '\\') {
-                ++i;
-                break;
-            }
-        }
-        return path.substr(i, std::string::npos);
-    }
-    
-    std::string replace(std::string str, char c1, char c2) {
-        std::string ret = str;
-        for (unsigned i = 0; i < str.length(); ++i) {
-            if (str[i] == c1) ret[i] = c2;
-        }
-        return ret;
-    }
+	std::string extractFilename(std::string path) {
+		int i = (int)path.size() - 1;
+		for (; i > 0; --i) {
+			if (path[i] == '/' || path[i] == '\\') {
+				++i;
+				break;
+			}
+		}
+		return path.substr(i, std::string::npos);
+	}
+	
+	std::string replace(std::string str, char c1, char c2) {
+		std::string ret = str;
+		for (unsigned i = 0; i < str.length(); ++i) {
+			if (str[i] == c1) ret[i] = c2;
+		}
+		return ret;
+	}
 }
 
 void MetalTranslator2::outputCode(const Target& target, const char* sourcefilename, const char* filename, std::map<std::string, int>& attributes) {
@@ -43,11 +43,11 @@ void MetalTranslator2::outputCode(const Target& target, const char* sourcefilena
 	}
 
 	spirv_cross::CompilerMSL* compiler = new spirv_cross::CompilerMSL(spirv);
-    
-    std::string name = extractFilename(sourcefilename);
-    name = name.substr(0, name.find_last_of("."));
-    name = replace(name, '-', '_');
-    name = replace(name, '.', '_');
+	
+	std::string name = extractFilename(sourcefilename);
+	name = name.substr(0, name.find_last_of("."));
+	name = replace(name, '-', '_');
+	name = replace(name, '.', '_');
 
 	compiler->set_entry_point("main");
 	compiler->set_entry_point_name(name);
